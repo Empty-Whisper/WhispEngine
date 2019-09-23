9163df3be4564308d9b40d8e1217da3b2046ebd7
Christian Martínez <christt105@gmail.com>
2019-09-23 12:43:46 +0200

    Comment tests

--- WhispEngine/WhispEngine/ModuleSceneIntro.cpp
+++ WhispEngine/WhispEngine/ModuleSceneIntro.cpp
@@ -6,6 +6,14 @@
 #include <random>
 #include "PCG/pcg_random.hpp"
 
+#include "MathGeoLib/include/MathGeoLib.h"
+#ifdef _DEBUG
+#pragma comment (lib, "MathGeoLib/lib_x86/Debug/MathGeoLib.lib")
+#else
+#pragma comment (lib, "MathGeoLib/lib_x86/Release/MathGeoLib.lib")
+#endif
+
+
 ModuleSceneIntro::ModuleSceneIntro(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
 }
@@ -25,6 +33,23 @@ bool ModuleSceneIntro::Start()
 	App->camera->Move(vec3(5.0f, 3.0f, 5.0f));
 	App->camera->LookAt(vec3(0.f, 0.f, 0.f));
 
+
+	//MathGeoLib test -----------------------------------------------
+	math::Sphere *a = new math::Sphere(math::float3(0.f, 0.f, 0.f), 5);
+	math::Sphere *b = new math::Sphere(math::float3(0.f, 0.f, 0.f), 5);
+	math::Sphere *c = new math::Sphere(math::float3(10.f, 10.f, 10.f), 1);
+
+	if (a->Intersects(*b)) {
+		LOG("A has intersection with B");
+	}
+	if (b->Intersects(*c)) {
+		LOG("B has intersection with C");
+	}
+	else {
+		LOG("B has no intersection with C");
+	}
+	//------------------------------------------------------------------
+
 	return ret;
 }
 
@@ -33,17 +58,18 @@ update_status ModuleSceneIntro::Update()
 {
 	plane->Render();
 
+	//PCG test --------------------------------------------------------
 	pcg_extras::seed_seq_from<std::random_device> seed_source;
 
 	pcg32 rng(seed_source);
 	std::uniform_int_distribution<int> uniform_dist_int(0, 10);
 	std::uniform_real_distribution<float> uniform_dist_f(0, 1);
 
-
 	for (int i = 0; i < 10; ++i) {
 		LOG("%i", uniform_dist_int(rng));
 		LOG("%f", uniform_dist_f(rng));
 	}
+	//-------------------------------------------------------------------
 	
 	return UPDATE_CONTINUE;
 }
