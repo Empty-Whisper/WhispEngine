f6e39b7e5f4b4107c50d856b52cc8550b8d2fde3
Marc Gálvez <36281630+optus23@users.noreply.github.com>
2019-09-25 22:02:26 +0200

    Mit license in about

--- WhispEngine/WhispEngine/ModuleGUI.cpp
+++ WhispEngine/WhispEngine/ModuleGUI.cpp
@@ -182,7 +182,21 @@ bool ModuleGUI::MenuWindowAbout(bool* p_open)
 		ImGui::Text("Whisp Engine is licensed under the MIT LICENSE, see LICENSE for more information.");
 		ImGui::NewLine();
 		ImGui::Checkbox("Show MIT LICENSE", &show_mit_license_window);
-		
+		if (show_mit_license_window)
+		{
+			ImGui::SetWindowSize(ImVec2(550, 485));
+			ImGui::NewLine(); ImGui::NewLine();
+			ImGui::Text("MIT LICENSE");	ImGui::NewLine();
+			ImGui::Text("Copyright (c) 2019 Empty-Whisper"); ImGui::NewLine();
+			ImGui::Text("Permission is hereby granted, free of charge, to any person obtaining a copy\nof this software and associated documentation files(the 'Software'), to deal\nin the Software without restriction, including without limitation the rights\nto use, copy, modify, merge, publish, distribute, sublicense, and/or sell\ncopies of the Software, and to permit persons to whom the Software is\nfurnished to do so, subject to the following conditions :"); ImGui::NewLine();
+			ImGui::Text("The above copyright notice and this permission notice shall be included in all\ncopies or substantial portions of the Software."); ImGui::NewLine();
+			ImGui::Text("THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\nIMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\nFITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE\nAUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\nLIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\nOUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\nSOFTWARE."); ImGui::NewLine();
+
+		}
+		else
+		{
+			ImGui::SetWindowSize(ImVec2(550, 150));
+		}
 			
 		
 	}
