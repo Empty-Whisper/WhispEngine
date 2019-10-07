4a56b167ed0545d373eaf20b545ad94449047f51
Christian Martínez <christt105@gmail.com>
2019-10-07 12:43:46 +0200

    Discovering ImGui::PushID()

--- WhispEngine/WhispEngine/PanelObjects.cpp
+++ WhispEngine/WhispEngine/PanelObjects.cpp
@@ -16,13 +16,15 @@ void PanelObjects::Update()
 {
 	if (ImGui::Begin("Inspector", &active)) {
 		auto vec = App->object_manager->GetObjects();
-		int cont = 0;
 		for (auto i = vec->begin(); i != vec->end(); ++i) {
-			ImGui::Checkbox(std::to_string(cont).data(), &(*i)->active); ImGui::SameLine(); ImGui::ColorEdit3(std::to_string(cont).data(), (*i)->color);
-			cont++;
+			ImGui::PushID(*i);
+			ImGui::Checkbox("", &(*i)->active); ImGui::SameLine(); 
+			if (ImGui::CollapsingHeader("Color")) {
+				ImGui::ColorEdit3("Face color", (*i)->color);
+				ImGui::ColorEdit3("Wire color", (*i)->wire_color);
+			}
+			ImGui::PopID();
 		}
-
-
 		ImGui::End();
 	}
 }
