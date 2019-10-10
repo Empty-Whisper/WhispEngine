421a736eeaa58bbe30428ff237f06969fc427aec
Christian Martínez <christt105@gmail.com>
2019-10-10 18:32:13 +0200

    Can Load textures dropping it in the window
    Put DevIL in module Import

---
+++ WhispEngine/WhispEngine/Texture.cpp
@@ -0,0 +1,13 @@
+#include "Texture.h"
+
+
+
+Texture::Texture(const uint &id, const char* name, const int& width, const int& height)
+	: id(id), name(name), width(width),height(height)
+{
+}
+
+
+Texture::~Texture()
+{
+}
