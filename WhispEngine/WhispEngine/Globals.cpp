aa1d82e9fee9919cf108b6e4db62d3ba80dfd985
Marc Gálvez <36281630+optus23@users.noreply.github.com>
2019-10-18 23:17:21 +0200

    Log cdirectory erased

--- WhispEngine/WhispEngine/Globals.cpp
+++ WhispEngine/WhispEngine/Globals.cpp
@@ -12,7 +12,7 @@ void log(const char file[], int line, const char* format, ...)
 	va_start(ap, format);
 	vsprintf_s(tmp_string, 4096, format, ap);
 	va_end(ap);
-	sprintf_s(tmp_string2, 4096, "\n%s(%d) : %s", file, line, tmp_string);
+	sprintf_s(tmp_string2, 4096, "\n%s",tmp_string);
 	OutputDebugString(tmp_string2); //Log in IDE Output window
 
 	if (App != nullptr) { //If App is available
