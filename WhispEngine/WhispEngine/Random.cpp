add434de6d27499a70cf0a41e042152ad229fdf5
Christian Martínez <christt105@gmail.com>
2019-11-06 22:22:52 +0100

    Random GUID

--- WhispEngine/WhispEngine/Random.cpp
+++ WhispEngine/WhispEngine/Random.cpp
@@ -1,10 +1,9 @@
 #include "Random.h"
 
-
-
 Random::Random()
 {
 	rng = pcg32(seed_source);
+	guid = std::uniform_real_distribution<double>(0, UINT64_MAX);
 }
 
 
@@ -23,4 +22,9 @@ int Random::Randomi(const int & min, const int & max)
 {
 	std::uniform_real_distribution<double> rand(min, max);
 	return rand(rng);
+}
+
+uint64_t Random::RandomGUID()
+{
+	return guid(rng);
 }
