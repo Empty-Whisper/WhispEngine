037eaaa2ee3ae8e447d440f501557615cdef32fb
Marc Gálvez <36281630+optus23@users.noreply.github.com>
2019-12-20 06:05:43 +0100

    Shot Tank logic added
    Commented future implementations

--- WhispEngine/Game/Assets/Scripts/ShotTank.lua
+++ WhispEngine/Game/Assets/Scripts/ShotTank.lua
@@ -13,11 +13,38 @@ ShotTank.Variables = {
 }
 
 -- local var = ShotTank.Variables
+local speed = 20
+local shot = true
+local prepare_timer = 0
+local time_disappear = 2
+--public GameObject shell
+--public GameObject canon
 
 function ShotTank:Start()
 	
 end
 
 function ShotTank:Update()
-	
+
+	if input.getKeyDown('q') then
+		--SetActive(true)
+		pos = transform.position
+		transform:SetPosition3f(pos.x,pos.y,pos.z)--Set pos = canon position, same for rotation?
+		--SetParent(spawnerBullets)
+		shot = true
+	end
+
+	if shot==true then
+		prepare_timer = prepare_timer + Time.deltaTime
+		--Translate forward
+	end
+
+	if prepare_timer >= time_disappear then
+		--SetActive(false)
+		shot = false
+		prepare_timer = 0
+	end
 end
+
+
+
