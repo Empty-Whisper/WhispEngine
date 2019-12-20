a0c9fb011f89104ea04b3e05539bdc5d54f5f41b
Marc Gálvez <36281630+optus23@users.noreply.github.com>
2019-12-20 04:55:53 +0100

    Tank rotates with wasd :ballot_box_with_check:

--- WhispEngine/Game/Assets/Scripts/MoveTank.lua
+++ WhispEngine/Game/Assets/Scripts/MoveTank.lua
@@ -15,7 +15,7 @@ function MoveTank:Start()
 end
 
 function MoveTank:Update()
---Log(Time.deltaTime)
+--Movement
 	if input.getKey('w') then
 		pos = transform.position
 		transform:SetPosition3f(pos.x,pos.y,pos.z-(speed*Time.deltaTime))
@@ -36,9 +36,31 @@ function MoveTank:Update()
 		transform:SetPosition3f(pos.x+(speed*Time.deltaTime),pos.y,pos.z)
 	end
 	
-	
-	
+--ChangeDirection
+	if input.getKeyDown('w') then
+		transform:SetRotation(Quaternion.FromEuler(0,math.rad(0),0))
+	end
+
+	if input.getKeyDown('a') then
+		transform:SetRotation(Quaternion.FromEuler(0,math.rad(90),0))
+	end
+
+	if input.getKeyDown('s') then
+		transform:SetRotation(Quaternion.FromEuler(0,math.rad(180),0))
+	end
+
+	if input.getKeyDown('d') then
+		transform:SetRotation(Quaternion.FromEuler(0,math.rad(275),0))
+	end
+
+
+
 end
+
+
+
+
+
 
 
 
