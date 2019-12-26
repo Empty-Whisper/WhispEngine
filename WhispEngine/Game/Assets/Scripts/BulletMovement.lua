e0aadef586ecbc66708635b7f77a641edf732db3
Marc Gálvez <36281630+optus23@users.noreply.github.com>
2019-12-26 23:21:04 +0100

    Revert, i didn't solve  timer :(

--- WhispEngine/Game/Assets/Scripts/BulletMovement.lua
+++ WhispEngine/Game/Assets/Scripts/BulletMovement.lua
@@ -5,27 +5,39 @@ BulletMovement = {}
 
 BulletMovement.Variables = {
 		-- Public Variables that will appear in inspector [All must be initialized]
+		cannon = nil, -- [GameObject]
+		prepare_timer = Time.time,
 		speed = 70,
 }
 
+-- local var = BulletMovement.Variables
+local shot = true
 local time_disappear = 2
-local prepare_timer = 0
+
 function BulletMovement:Start()
-	var.prepare_timer = 0
-
-
+	--GameObject.SetParent(var.cannon)
+	transform.position = cannon.transform.position
+	transform.rotation = cannon.transform.rotation
+	var.prepare_timer = Time.time
+	
 end
 
 function BulletMovement:Update()
+
+	--prepare_timer = prepare_timer + Time.deltaTime
+	Log(var.prepare_timer)
 	pos = transform.position
 	transform:SetPositionv(pos + (transform.forward * (var.speed*Time.deltaTime)))
-	prepare_timer = prepare_timer + Time.deltaTime
 
-	if prepare_timer >= time_disappear then
+
+	if Time.time - var.prepare_timer >= time_disappear then
 		GameObject.Destroy(object)
 	end
 
 end
+
+
+
 
 
 
